66152e5982c94b1387d1e9d7b4f089a32f17cd49
Andressa Colin <104472710+AndressaColin@users.noreply.github.com>
2022-06-24 19:25:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,79 +1,28 @@
 
-//UPDATE TIMER
-
 const timer = {
   pomodoro: 25,
   shortBreak: 5,
   longBreak: 15,
   longBreakInterval: 4,
+  sessions: 0,
 };
-
-//CLICK ON THE BUTTONS
-
-const modeButtons = document.querySelector('#js-mode-buttons');
-modeButtons.addEventListener('click', handleMode);
-
-function handleMode(event) {
-  const { mode } = event.target.dataset;
-
-  if (!mode) return;
-
-  switchMode(mode);
-}
-
-//DETECT A CLICK 
-
-function switchMode(mode) {
-  timer.mode = mode;
-  timer.remainingTime = {
-    total: timer[mode] * 60,
-    minutes: timer[mode],
-    seconds: 0,
-  };
-
-  // total É O TOTAL DE SEGUNDOS CONTADOS. SESSAO DE MINUTOS MULTIPLICADO POR 60. EX: IF mode É UMA pausa pequena, total SERÁ 300 (5X60)
-
-  document
-    .querySelectorAll('button[data-mode]')
-    .forEach(e => e.classList.remove('active'));
-  document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
-  document.body.style.backgroundColor = `var(--${mode})`;
-
-  updateClock();
-}
-
-//update clock EXTRAI O VALOR DE minutes E seconds PROPRIEDADES DO OBJETO remmainingTime 
-
-function updateClock() {
-  const { remainingTime } = timer;
-  const minutes = `${remainingTime.minutes}`.padStart(2, '0');
-  const seconds = `${remainingTime.seconds}`.padStart(2, '0');
-
-  const min = document.getElementById('js-minutes');
-  const sec = document.getElementById('js-seconds');
-  min.textContent = minutes;
-  sec.textContent = seconds;
-}
-
-//COMEÇAR O TIMER 
 
 let interval;
 
-function startTimer() {
-  let { total } = timer.remainingTime;
-  const endTime = Date.parse(new Date()) + total * 1000;
+const buttonSound = new Audio('button-sound.mp3');
+const mainButton = document.getElementById('js-btn');
+mainButton.addEventListener('click', () => {
+  buttonSound.play();
+  const { action } = mainButton.dataset;
+  if (action === 'start') {
+    startTimer();
+  } else {
+    stopTimer();
+  }
+});
 
-  interval = setInterval(function() {
-    timer.remainingTime = getRemainingTime(endTime);
-    updateClock();
-
-    total = timer.remainingTime.total;
-    if (total <= 0) {
-      clearInterval(interval);
-    }
-  }, 1000);
-}
-
+const modeButtons = document.querySelector('#js-mode-buttons');
+modeButtons.addEventListener('click', handleMode);
 
 function getRemainingTime(endTime) {
   const currentTime = Date.parse(new Date());
@@ -90,77 +39,6 @@ function getRemainingTime(endTime) {
   };
 }
 
-const mainButton = document.getElementById('js-btn');
-mainButton.addEventListener('click', () => {
-  const { action } = mainButton.dataset;
-  if (action === 'start') {
-    startTimer();
-  }
-});
-
-function startTimer() {
-  let { total } = timer.remainingTime;
-  const endTime = Date.parse(new Date()) + total * 1000;
-
-  mainButton.dataset.action = 'stop';
-  mainButton.textContent = 'stop';
-  mainButton.classList.add('active');
-
-  interval = setInterval(function() {
-    timer.remainingTime = getRemainingTime(endTime);
-    updateClock();
-
-    total = timer.remainingTime.total;
-    if (total <= 0) {
-      clearInterval(interval);
-    }
-  }, 1000);
-}
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  switchMode('pomodoro');
-});
-
-//PARAR O TIMER
-
-function stopTimer() {
-  clearInterval(interval);
-
-  mainButton.dataset.action = 'start';
-  mainButton.textContent = 'start';
-  mainButton.classList.remove('active');
-}
-
-
-mainButton.addEventListener('click', () => {
-  const { action } = mainButton.dataset;
-  if (action === 'start') {
-    startTimer();
-  } else {
-    stopTimer();
-  }
-});
-
-function handleMode(event) {
-  const { mode } = event.target.dataset;
-
-  if (!mode) return;
-
-  switchMode(mode);
-  stopTimer();
-}
-
-//COMEÇAR A PROXIMA SESSAO AUTOMATICO 
-
-const timer = {
-  pomodoro: 25,
-  shortBreak: 5,
-  longBreak: 15,
-  longBreakInterval: 4,
-  sessions: 0,
-};
-
 function startTimer() {
   let { total } = timer.remainingTime;
   const endTime = Date.parse(new Date()) + total * 1000;
@@ -175,26 +53,6 @@ function startTimer() {
     timer.remainingTime = getRemainingTime(endTime);
     updateClock();
 
-    total = timer.remainingTime.total;
-    if (total <= 0) {
-      clearInterval(interval);
-    }
-  }, 1000);
-}
-
-function startTimer() {
-  let { total } = timer.remainingTime;
-  const endTime = Date.parse(new Date()) + total * 1000;
-
-  if (timer.mode === 'pomodoro') timer.sessions++;
-
-  mainButton.dataset.action = 'stop';
-  mainButton.textContent = 'stop';
-  mainButton.classList.add('active');
-
-  interval = setInterval(function() {
-    timer.remainingTime = getRemainingTime(endTime);
-    updateClock();
     total = timer.remainingTime.total;
     if (total <= 0) {
       clearInterval(interval);
@@ -211,12 +69,44 @@ function startTimer() {
           switchMode('pomodoro');
       }
 
+      if (Notification.permission === 'granted') {
+        const text =
+          timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+        new Notification(text);
+      }
+
+      document.querySelector(`[data-sound="${timer.mode}"]`).play();
+
       startTimer();
     }
   }, 1000);
 }
 
-//ATUALIZAR O PROGRESSO 
+function stopTimer() {
+  clearInterval(interval);
+
+  mainButton.dataset.action = 'start';
+  mainButton.textContent = 'start';
+  mainButton.classList.remove('active');
+}
+
+function updateClock() {
+  const { remainingTime } = timer;
+  const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+  const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+
+  const min = document.getElementById('js-minutes');
+  const sec = document.getElementById('js-seconds');
+  min.textContent = minutes;
+  sec.textContent = seconds;
+
+  const text =
+    timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+  document.title = `${minutes}:${seconds} — ${text}`;
+
+  const progress = document.getElementById('js-progress');
+  progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
+}
 
 function switchMode(mode) {
   timer.mode = mode;
@@ -238,71 +128,23 @@ function switchMode(mode) {
   updateClock();
 }
 
+function handleMode(event) {
+  const { mode } = event.target.dataset;
 
-function updateClock() {
-  const { remainingTime } = timer;
-  const minutes = `${remainingTime.minutes}`.padStart(2, '0');
-  const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+  if (!mode) return;
 
-  const min = document.getElementById('js-minutes');
-  const sec = document.getElementById('js-seconds');
-  min.textContent = minutes;
-  sec.textContent = seconds;
-
-  const progress = document.getElementById('js-progress');
-  progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
+  switchMode(mode);
+  stopTimer();
 }
-
-//REFLETIR A CONTAGEM
-
-
-function updateClock() {
-  const { remainingTime } = timer;
-  const minutes = `${remainingTime.minutes}`.padStart(2, '0');
-  const seconds = `${remainingTime.seconds}`.padStart(2, '0');
-
-  const min = document.getElementById('js-minutes');
-  const sec = document.getElementById('js-seconds');
-  const time = `${minutes}:${seconds}`;
-  min.textContent = minutes;
-  sec.textContent = seconds;
-
-  const text = timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
-  document.title = `${minutes}:${seconds} — ${text}`;
-
-  const progress = document.getElementById('js-progress');
-  progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
-}
-
-//SONS
-
-const buttonSound = new Audio('button-sound.mp3');
-const mainButton = document.getElementById('js-btn');
-mainButton.addEventListener('click', () => {
-  buttonSound.play();
-  const { action } = mainButton.dataset;
-  if (action === 'start') {
-    startTimer();
-  } else {
-    stopTimer();
-  }
-});
-
-document.querySelector(`[data-sound="${timer.mode}"]`).play();
-
-
-//display notificações 
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Let's check if the browser supports notifications
   if ('Notification' in window) {
-    // If notification permissions have neither been granted or denied
-    if (Notification.permission !== 'granted' && Notification.permission !== 'denied') {
-      // ask the user for permission
+    if (
+      Notification.permission !== 'granted' &&
+      Notification.permission !== 'denied'
+    ) {
       Notification.requestPermission().then(function(permission) {
-        // If permission is granted
         if (permission === 'granted') {
-          // Create a new notification
           new Notification(
             'Awesome! You will be notified at the start of each session'
           );
@@ -313,30 +155,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
   switchMode('pomodoro');
 });
-
-
-if (Notification.permission === 'granted') {
-  const text =
-    timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
-  new Notification(text);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
